1fc2cca898a9fa2a6b5b80d7a6dc988b002c1b04
cauagrc <cauagaldinod99@gmail.com>
2026-04-23 10:15:35 -0400

    feat: Adiciona import message no modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PermutacoesModule } from './permutacoes/permutacoes.module';
 import { SolicitacoesModule } from './solicitacoes/solicitacoes.module';
 import { ForunsModule } from './foruns/foruns.module';
 import { ConversasModule } from './conversas/conversas.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [UsuariosModule,ForunsModule, AutorizacoesModule, InstituicoesModule, PermutacoesModule, SolicitacoesModule, ForunsModule, ConversasModule],
+  imports: [UsuariosModule,ForunsModule, AutorizacoesModule, InstituicoesModule, PermutacoesModule, SolicitacoesModule, ForunsModule, ConversasModule, MessagesModule],
   providers: [PrismaService],
 })
 export class AppModule {}
